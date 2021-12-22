3598fdb3330ee746a266684da713c6827640f9e9
jiteshsavdekar <jitusavdekar1996@gmail.com>
2021-12-22 12:40:57 +0530

    Add

---
+++ calu/src/app/calc/calc.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent implements OnInit {
+ result ? : number=0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  plus(n1 :string,n2 :string):void{
+    //alert(parseInt(n1)+parseInt(n2));
+    this.result=parseInt((n1)+(n2));
+  }
+  minus(n1 :string,n2 :string):void{
+    //alert(parseInt(n1)-parseInt(n2));
+    this.result=parseInt(n1)-parseInt(n2);
+  }
+  mul(n1 :string,n2 :string):void{
+    //alert(parseInt(n1)*parseInt(n2));
+    this.result=parseInt(n1)*parseInt(n2);
+  }
+  div(n1 :string,n2 :string):void{
+    //alert(parseInt(n1)/parseInt(n2));
+    this.result=parseInt(n1)/parseInt(n2);
+  }
+}
